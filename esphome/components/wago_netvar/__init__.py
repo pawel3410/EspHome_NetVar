14ec309e12c2dbc1f3c2cbfe049f38045edbf014
pawel3410 <pawel3410@gmail.com>
2026-08-26 15:51:29 +0200

    Update __init__.py

--- esphome/components/wago_netvar/__init__.py
+++ esphome/components/wago_netvar/__init__.py
@@ -89,7 +89,6 @@ CONFIG_SCHEMA = (
     .extend(cv.polling_component_schema("10s"))
 )
 
-
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
